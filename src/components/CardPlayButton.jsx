e51ec4a108d8587c6c9cf22ee7368dc940d242ff
Santi <canosantiago404@gmail.com>
2024-04-06 14:16:17 -0300

    boton de play individual

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,20 +1,28 @@
+import { playlists } from "@/lib/data";
 import { Pause, Play } from "./Player";
 import { usePlayerStore } from "@/store/playerStore";
 
 export function CardPlayButton({ id }) {
-  const { currentSong, isPlaying, setIsPlaying, setCurrentSong } =
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
 
   const handleClick = () => {
+    setCurrentMusic({
+      playlist: {
+        id,
+      },
+    });
     setIsPlaying(!isPlaying);
   };
+
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
 
   return (
     <button
       onClick={handleClick}
       className="card-play-button rounded-full bg-green-500 p-4"
     >
-      {isPlaying ? <Pause /> : <Play />}
+      {isPlayingPlaylist ? <Pause /> : <Play />}
     </button>
   );
 }
